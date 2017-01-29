005f7fec1a6912f1a339a784920c87e5b7ff1621
r.palmin <roman.palmin@gmail.com>
2017-01-29 23:46:53 +0300

    test

--- src/components/ProFooter.vue
+++ src/components/ProFooter.vue
@@ -5,7 +5,7 @@
             <div class="email">email</div>
             <div class="phone">phone</div>
             <div class="social">social</div>
-{{msg}}
+            {{msg}}
         </footer>
     </div>
 </template>
@@ -13,30 +13,30 @@
     .container {
         width: 88%;
 
-        footer {
-            background-color: #d8ffee;
-            height: 20px;
-            width: 100%;
-            margin: auto;
-            text-align: center;
-            padding-top: 2px;
+    footer {
+        background-color: #d8ffee;
+        height: 20px;
+        width: 100%;
+        margin: auto;
+        text-align: center;
+        padding-top: 2px;
 
-            div {
-                float: left;
-                font-size: 1.2em;
+    div {
+        float: left;
+        font-size: 1.2em;
 
-                .copyright {
-                    background-color: #2e2e2e;
-                    width: 20%;
-                }
+    .copyright {
+        background-color: #2e2e2e;
+        width: 20%;
+    }
 
-                .email {
-                    background-color: #2e2e2e;
-                    width: 20%;
-                }
+    .email {
+        background-color: #2e2e2e;
+        width: 20%;
+    }
 
-            }
-        }
+    }
+    }
     }
 </style>
 <script>
